ccaa881513656194e8a777a3db3d8796c4012360
Generosa Litton <glcodeworks@gmail.com>
2021-02-28 15:52:05 -0800

    added children to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <main>{children}</main>
+      <Footer />
     </>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,13 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
-      <Layout />
+    <Layout>
       Hello World, It's Me!
       <div>
         <Link to="/blog">Blog page</Link>
         <a href="/products">Products page</a>
       </div>
       <a href="https://www.gatsbyjs.org">Gatsby</a>
-    </div>
+    </Layout>
   );
 }
